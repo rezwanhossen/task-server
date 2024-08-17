727430f94fe97dd63a983d4863c02cfd1763cba4
rezwanhossen <rezwanhossen625@gmail.com>
2024-08-18 02:30:54 +0600

    get all data

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const app = express();
 //meddil wair =======
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5001"],
   })
 );
 app.use(express.json());
-//W6WPo7VYaErCsEkm
-//jobTask
+
 //=================================================
 
 // const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -30,16 +29,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    const productCol = client.db("job-task").collection("product");
+
+    // await client.connect();
+    app.get("/product", async (req, res) => {
+      const result = await productCol.find().toArray();
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
